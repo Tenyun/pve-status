fc468bb7105f560af4a1d49fc023e5b204f08728
Dustin Hutto <huttodustin@gmail.com>
2018-10-13 11:14:23 +0200

    Change all output language to english

--- status.sh
+++ status.sh
@@ -39,37 +39,37 @@ printHeadLine(){
 
 printf "\\ec"
 
-printHeadLine "System-Info"
+printHeadLine "SYSTEM INFO"
 echo -e System OS:'\t'"Debian"
 echo -e Kernel:'\t\t'"$(uname -r)"
 echo -e Uptime:'\t\t'"$(uptime -p)"
 echo -e CPU:'\t\t'"$(< /proc/cpuinfo awk '/model name/{print $4, $5, $6, $7, $8, $9, $10; exit}')"
 echo -e ""
-echo "## Temperaturen CPU ##"
+echo "## CPU temperature ##"
 echo -e ""
 sensors | awk '/Core|Package/' | cut -d "(" -f 1
 echo -e ""
-echo "## Speicherauslastung ##"
+echo "## Memory usage ##"
 echo ""
 free -h
 echo -e ""
 
-printHeadLine "ZFS-Filesystem"
+printHeadLine "ZFS STATUS"
 get_zfs_data
-echo "## Pool-Status ##"
+echo "## Pool status ##"
 echo -e ""
 echo "$var_zfs_pool_status" | awk 'BEGIN{OFS = "-"; print "Pool-Name""\t""Status""\t""Last-Scrub""\t""\t""Repaired-Errors""\t""Scrub-Errors""\t""Time""\t""Data-Errors"}; /pool/{ POOL=$2; next} /state/{STATE=$2; next} /scan/{SCAN1=$12"-"$13"-"$14"-"$15; SCAN2=$4; SCAN3=$8; SCAN4=$6; next} /error/{$1=""; print POOL"\t"STATE"\t"SCAN1"\t"SCAN2"\t"SCAN3"\t"SCAN4"\t"$0}' | column -t
 echo -e ""
 
 echo "## Snapshots ##"
 echo -e ""
-echo Letzte Snapshots
-echo -e "Stündlich \\tTäglich \\tMonatlich \\t\\tJährlich"
+echo Last zfs snapshots
+echo -e "hourly \\tdaily \\tmonthly \\t\\tyearly"
 echo -e "$var_zfs_last_snapshot_hourly \\t$var_zfs_last_snapshot_daily \\t$var_zfs_last_snapshot_monthly \\t$var_zfs_last_snapshot_yearly"
 echo -e ""
-echo Anzahl aller Snapschots: "$var_zfs_snapshots_count"
+echo Total snapschot count: "$var_zfs_snapshots_count"
 echo -e ""
 
 echo "## mainpool Capacity  ##"
 echo -e ""
-echo -e Belegter Speicherplatz:'\n'"$var_zfs_cap_mainpool"
+echo -e Used zfs capacity:'\n'"$var_zfs_cap_mainpool"
