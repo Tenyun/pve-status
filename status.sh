8fe83d32b216f06f52d4bc86ebde98b46b14ebe3
Tenyun <tenyun@pve1.lan>
2018-11-14 19:46:58 +0100

    change all outputs to work with printf

--- status.sh
+++ status.sh
@@ -22,11 +22,11 @@ set -o nounset                                  # Treat unset variables as an er
 get_zfs_data() {
 var_zfs_last_snapshot_all=$(zfs list -H -t snapshot -o name -S creation)
 var_zfs_pool_status=$(zpool status | grep -E "(pool:|state:|scan:|errors:)" )
-var_zfs_last_snapshot_hourly=$(echo "$var_zfs_last_snapshot_all" | awk -F_ '/hourly/{print $(NF-1);exit;}')
-var_zfs_last_snapshot_daily=$(echo "$var_zfs_last_snapshot_all" | awk -F_ '/daily/{print $(NF-1);exit;}')
-var_zfs_last_snapshot_monthly=$(echo "$var_zfs_last_snapshot_all" | awk -F_ '/monthly/{print $(NF-2)"_"$(NF-1);exit;}')
-var_zfs_last_snapshot_yearly=$(echo "$var_zfs_last_snapshot_all" | awk -F_ '/yearly/{print $(NF-2)"_"$(NF-1);exit;}')
-var_zfs_snapshots_count=$(echo "$var_zfs_last_snapshot_all" | awk 'END {print NR}')
+var_zfs_last_snapshot_hourly=$(awk -F_ '/hourly/{print $(NF-1);exit;}' <<< "$var_zfs_last_snapshot_all")
+var_zfs_last_snapshot_daily=$(awk -F_ '/daily/{print $(NF-1);exit;}' <<< "$var_zfs_last_snapshot_all")
+var_zfs_last_snapshot_monthly=$(awk -F_ '/monthly/{print $(NF-2)"_"$(NF-1);exit;}' <<< "$var_zfs_last_snapshot_all")
+var_zfs_last_snapshot_yearly=$(awk -F_ '/yearly/{print $(NF-2)"_"$(NF-1);exit;}' <<< "$var_zfs_last_snapshot_all")
+var_zfs_snapshots_count=$(awk 'END {print NR}' <<< "$var_zfs_last_snapshot_all")
 var_zfs_cap_mainpool=$(zpool list -H -o name,capacity)
 }
 
@@ -42,36 +42,31 @@ printHeadLine(){
 printf "\\ec"
 
 printHeadLine "SYSTEM INFO"
-echo -e System OS:'\t'"Debian"
-echo -e Kernel:'\t\t'"$(uname -r)"
-echo -e Uptime:'\t\t'"$(uptime -p)"
-echo -e CPU:'\t\t'"$(< /proc/cpuinfo awk '/model name/{print $4, $5, $6, $7, $8, $9, $10; exit}')"
-echo -e ""
-echo "## CPU temperature ##"
-echo -e ""
+printf "%-14s %s\n" "System:" "Debian"
+printf "%-14s %s\n" "Kernel:" "$(uname -r)"
+printf "%-14s %s\n" "Uptime:" "$(uptime -p)"
+printf "%-14s %s\n\n" "CPU:" "$(< /proc/cpuinfo awk '/model name/{print $4, $5, $6, $7, $8, $9, $10; exit}')"
+
+printf "## CPU Temperature ##\n\n"
 sensors | awk -F\( '/Core|Package/{print $(NF-1)}'
-echo -e ""
-echo "## Memory usage ##"
-echo ""
+printf "\n"
+printf "## Memory usage ##\n\n"
 free -h
-echo -e ""
+printf "\n"
 
 printHeadLine "ZFS STATUS"
 get_zfs_data
-echo "## Pool status ##"
-echo -e ""
-echo "$var_zfs_pool_status" | awk 'BEGIN{OFS = "-"; print "Pool-Name""\t""Status""\t""Last-Scrub""\t""\t""Repaired-Errors""\t""Scrub-Errors""\t""Time""\t""Data-Errors"}; /pool/{ POOL=$2; next} /state/{STATE=$2; next} /scan/{SCAN1=$12"-"$13"-"$14"-"$15; SCAN2=$4; SCAN3=$8; SCAN4=$6; next} /error/{$1=""; print POOL"\t"STATE"\t"SCAN1"\t"SCAN2"\t"SCAN3"\t"SCAN4"\t"$0}' | column -t
-echo -e ""
+printf "## Pool status ##\n\n"
+awk 'BEGIN{OFS = "-"; print "Pool-Name""\t""Status""\t""Last-Scrub""\t""\t""Repaired-Errors""\t""Scrub-Errors""\t""Time""\t""Data-Errors"}; 
+/pool/{ POOL=$2; next} /state/{STATE=$2; next} /scan/{SCAN1=$12"-"$13"-"$14"-"$15; SCAN2=$4; SCAN3=$8; SCAN4=$6; next} /error/{$1=""; print POOL"\t"STATE"\t"SCAN1"\t"SCAN2"\t"SCAN3"\t"SCAN4"\t"$0}' <<< "$var_zfs_pool_status" | column -t
 
-echo "## Snapshots ##"
-echo -e ""
-echo Last zfs snapshots
-echo -e "hourly \\tdaily \\tmonthly \\t\\tyearly"
-echo -e "$var_zfs_last_snapshot_hourly \\t$var_zfs_last_snapshot_daily \\t$var_zfs_last_snapshot_monthly \\t$var_zfs_last_snapshot_yearly"
-echo -e ""
-echo Total snapschot count: "$var_zfs_snapshots_count"
-echo -e ""
+printf "\n## Snapshots ##\n\n"
+printf "Last zfs snapshots\n"
+printf "%-10s %s\n" "HOURLY:" "$var_zfs_last_snapshot_hourly"
+printf "%-10s %s\n" "DAILY:" "$var_zfs_last_snapshot_daily"
+printf "%-10s %s\n" "MONTHLY:" "$var_zfs_last_snapshot_monthly"
+printf "%-10s %s\n\n" "YEARLY:" "$var_zfs_last_snapshot_yearly"
+printf "%-10s %s\n\n" "Total Snapshots:" "$var_zfs_snapshots_count"
 
-echo "## mainpool Capacity  ##"
-echo -e ""
-echo -e Used zfs capacity:'\n'"$var_zfs_cap_mainpool"
+printf "## Pool Capacity\'s  ##\n\n"
+printf "%s\n" "$var_zfs_cap_mainpool"
